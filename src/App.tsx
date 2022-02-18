da719537bfe456906e20826f7083ce76b465dd73
gene222-arch <genephillip222@gmail.com>
2022-02-19 01:46:03 +0800

    feat: conditional layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,42 @@
 import { CssBaseline } from '@mui/material';
+import { createStructuredSelector } from 'reselect';
 import { privateRoutes, publicRoutes } from './routes';
 import RenderRoute from './routes/RenderRoute';
 import AuthLayout from './views/layouts/AuthLayout';
 import DashboardLayout from './views/layouts/DashboardLayout';
+import { authSelector } from './redux/auth/selectors';
+import { connect } from 'react-redux';
+import { AuthState } from './types/states/auth/AuthState';
 
-const App = () => 
+interface Prop {
+    authState: AuthState
+}
+
+const App = ({ authState }: Prop) => 
 {
     return (
         <div>
             <CssBaseline />
-            <AuthLayout>
-                <RenderRoute routes={ publicRoutes } />
-            </AuthLayout>
-            <DashboardLayout>
-                <RenderRoute routes={ privateRoutes } />
-            </DashboardLayout>
+            {
+                !authState.isAuthenticated && (
+                    <AuthLayout>
+                        <RenderRoute routes={ publicRoutes } />
+                    </AuthLayout>
+                )
+            }
+            {
+                authState.isAuthenticated && (
+                    <DashboardLayout>
+                        <RenderRoute routes={ privateRoutes } />
+                    </DashboardLayout>
+                )
+            }
         </div>
     )  
 };
 
-export default App;
+const mapStateToProps = createStructuredSelector({
+    authState: authSelector 
+});
+
+export default connect(mapStateToProps)(App);
