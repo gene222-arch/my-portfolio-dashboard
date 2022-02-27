c0e6d0237665ebb5127c511c63fd965ce7e147a9
gene222-arch <genephillip222@gmail.com>
2022-02-27 08:58:58 +0800

    feat: add cards with data

---
+++ src/components/dashboard/Card.tsx
@@ -0,0 +1,45 @@
+import { Card as MuiCard, CardContent, Grid, IconButton, SvgIconTypeMap, Typography } from '@mui/material';
+import { OverridableComponent } from '@mui/material/OverridableComponent';
+
+interface Prop {
+    label: string,
+    value: string | number,
+    icon: OverridableComponent<SvgIconTypeMap<{}, "svg">> & {
+        muiName: string;
+    }
+}
+
+const Card = ({ label, value, icon: Icon }: Prop) => 
+{
+    return (
+        <MuiCard 
+            sx={{ 
+                width: '100%', 
+                height: '23vh',
+                background: `linear-gradient(to bottom, #2c2c2c 1%, #121212 100%);` 
+            }}
+        >
+            <CardContent>
+                <Grid container spacing={ 2 } flexDirection='column' alignItems='center'>
+                    <Grid item>
+                        <IconButton>
+                            <Icon fontSize='large' />
+                        </IconButton>
+                    </Grid>
+                    <Grid item>
+                        <Typography variant="body2" color='GrayText'>
+                            { label }
+                        </Typography>
+                    </Grid>
+                    <Grid item>
+                        <Typography variant="h6" sx={{ color: '#ec5b53' }}>
+                            <strong>{ value }</strong>
+                        </Typography>
+                    </Grid>
+                </Grid>
+            </CardContent>
+        </MuiCard>
+    );
+};
+
+export default Card;
--- src/views/app/DashboardPage.tsx
+++ src/views/app/DashboardPage.tsx
@@ -1,8 +1,59 @@
+import Container from '@mui/material/Container'
+import Grid from '@mui/material/Grid'
+import Card from '../../components/dashboard/Card';
+import RemoveRedEyeOutlinedIcon from '@mui/icons-material/RemoveRedEyeOutlined';
+import PlaylistAddCheckCircleOutlinedIcon from '@mui/icons-material/PlaylistAddCheckCircleOutlined';
+import { Divider } from '@mui/material';
 
-const DashboardPage = () => {
-  return (
-    <div>DashboardPage</div>
-  );
+const defaultData = [
+	{
+		label: 'Views',
+		value: '100, 530',
+		icon: RemoveRedEyeOutlinedIcon
+	},
+	{
+		label: 'Projects',
+		value: 15,
+		icon: PlaylistAddCheckCircleOutlinedIcon
+	},
+	{
+		label: 'Views',
+		value: '100, 530',
+		icon: RemoveRedEyeOutlinedIcon
+	},
+	{
+		label: 'Projects',
+		value: 15,
+		icon: PlaylistAddCheckCircleOutlinedIcon
+	},
+	{
+		label: 'Views',
+		value: '100, 530',
+		icon: RemoveRedEyeOutlinedIcon
+	},
+	{
+		label: 'Projects',
+		value: 15,
+		icon: PlaylistAddCheckCircleOutlinedIcon
+	}
+];
+
+const DashboardPage = () => 
+{
+    return (
+        <Container maxWidth="lg">
+            <Grid container spacing={ 4 } justifyContent='center'>
+				{
+					defaultData.map((data, index) => (
+						<Grid item xs={ 12 } sm={ 2 } key={ index }>
+							<Card { ...data } />
+						</Grid>
+					))
+				}
+            </Grid>
+			<Divider sx={{ height: '0.05rem', backgroundColor: '#FFFFFF' }} />
+        </Container>
+    );
 };
 
 export default DashboardPage
