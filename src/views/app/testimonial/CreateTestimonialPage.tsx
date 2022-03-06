c5ebf01e94fc0727e5529451fd970146d5cee2d8
gene222-arch <genephillip222@gmail.com>
2022-03-06 17:02:26 +0800

    feat: implement create testimonial api

--- src/views/app/testimonial/CreateTestimonialPage.tsx
+++ src/views/app/testimonial/CreateTestimonialPage.tsx
@@ -1,6 +1,8 @@
 import React, { useState } from 'react'
 import InputFieldSection from '../../../components/testimonial/InputFieldSection';
 import { TestimonialItemType } from '../../../types/states/testimonial/TestimonialState';
+import { useDispatch } from 'react-redux';
+import { createTestimonialStart } from '../../../redux/testimonial/action.creators';
 
 const defaultTestimonial: TestimonialItemType = {
     name: '',
@@ -11,12 +13,13 @@ const defaultTestimonial: TestimonialItemType = {
 
 const CreateTestimonialPage = () => 
 {
+    const dispatch = useDispatch();
     const [ testimonial, setTestimonial ] = useState(defaultTestimonial);
 
     const handleOnSubmit = (e: React.FormEvent<HTMLFormElement>) => 
     {
         e.preventDefault();
-        window.alert('Submitted');
+        dispatch(createTestimonialStart(testimonial));
     };
 
     return (
