098d4917bbdd9060a9731a99c5b068dd8e0b46fb
gene222-arch <genephillip222@gmail.com>
2022-03-28 05:15:35 +0800

    feat: implement getEmailsStart

--- src/apis/email.ts
+++ src/apis/email.ts
@@ -2,10 +2,10 @@ import { GetEmailsSuccessResponse, GetEmailsFailedResponse } from 'types/states/
 import { GetEmailPayload } from 'types/states/email/GetEmailPayload';
 import axiosInstance from '../utils/axiosInstance';
 
-export const index = async (payload: GetEmailPayload): Promise<GetEmailsSuccessResponse | GetEmailsFailedResponse> => 
+export const index = async ({ archive }: GetEmailPayload): Promise<GetEmailsSuccessResponse | GetEmailsFailedResponse> => 
 {
     return await axiosInstance()
-        .get('/emails')
+        .get(`/emails?archives=${ archive }`)
         .then((response: { data: GetEmailsSuccessResponse }) => response.data)
         .catch((error: { response: { data: GetEmailsFailedResponse }}) => Promise.reject(error.response.data))
 };
--- src/views/app/email/index.tsx
+++ src/views/app/email/index.tsx
@@ -1,26 +1,60 @@
-import { GridColDef } from '@mui/x-data-grid';
+import { IconButton, Tooltip } from '@mui/material';
+import { GridColDef, GridRenderCellParams } from '@mui/x-data-grid';
 import DataGridComponent from 'components/DataGridComponent';
+import { useEffect, useState } from 'react';
+import { useDispatch, connect } from 'react-redux';
+import { getEmailsStart } from 'redux/email/action.creators';
+import { emailSelector } from 'redux/email/selectors';
+import { createStructuredSelector } from 'reselect';
+import { EmailState } from 'types/states/email/EmailState';
+import VisibilityTwoToneIcon from '@mui/icons-material/VisibilityTwoTone';
 
 const columns: GridColDef[] = [
-    { field: 'id', hide: true },
-    { field: 'name', headerName: 'Name', width: 315 },
-    { field: 'email', headerName: 'Email', width: 315 },
-    { field: 'message', headerName: 'Message', width: 500 },
-    { field: 'created_at', headerName: 'Mailed at', width: 200 },
+    { field: 'name', headerName: 'Name', width: 300 },
+    { field: 'email', headerName: 'Email', width: 300 },
+    { field: 'message', headerName: 'Message', width: 412 },
+    { field: 'created_at', headerName: 'Mailed at', width: 300 },
+    { 
+        field: 'action buttons', 
+        headerName: 'Action', 
+        width: 100,
+        renderCell: (params: GridRenderCellParams<any, any, any>) => (
+            <Tooltip title='View More Details' placement='top-start'>
+                <IconButton>
+                    <VisibilityTwoToneIcon color='info' />
+                </IconButton>
+            </Tooltip>
+        ),
+    }
 ];
 
-const Email = () => 
+interface Prop {
+    emailState: EmailState
+}
+
+const Email = ({ emailState }: Prop) => 
 {
+    const dispatch = useDispatch();
+    const [ isArchived, setIsArchived ] = useState(false);
+
+    useEffect(() => {
+        dispatch(getEmailsStart({ archive: isArchived }));
+    }, []);
+
     return (
         <DataGridComponent
             columns={ columns }
-            rows={ [] }
+            rows={ emailState.emails }
             onCellClick={ () => 1 }
             onClickAddButton={ () => 1 }
             addButtonTooltipTitle=''
-            isLoading={ false }
+            isLoading={ emailState.isLoading }
         />
     );
 };
 
-export default Email;
+const mapStateToProps = createStructuredSelector({
+    emailState: emailSelector
+});
+
+export default connect(mapStateToProps)(Email);
